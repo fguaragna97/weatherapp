ee577b6f167163e60369d1ed9f6f334bc9970bfa
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2021-10-01 20:30:15 -0700

    Replace Information in the html from the api

---
+++ index.js
@@ -0,0 +1,39 @@
+// Make the page load
+window.addEventListener("load", () => {
+  // define variables
+  let longitude;
+  let latitude;
+  // if the user allow the geo location then
+  if (navigator.geolocation) {
+    function success(pos) {
+      coordinates = pos.coords;
+      longitude = coordinates.longitude;
+      latitude = coordinates.latitude;
+
+      // Api call OpenWeathermap
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=09907f7848d7e14ba1a36ebd01699e57&units=metric`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // get elements and replace the value with the api value
+          const temp = document.getElementById("temp");
+          temp.textContent = Math.floor(data.main.temp);
+
+          const city = document.getElementById("city");
+          city.textContent = data.name;
+
+          const icon = document.getElementById("icon");
+          icon.innerHTML = `<img src=\"http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+
+          const description = document.getElementById("description");
+          description.textContent = data.weather[0].description;
+        });
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
+  }
+  // if the user declines the geo location
+  else {
+  }
+});
